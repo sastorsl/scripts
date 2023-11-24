143e00bfe65db9656a516101393d59124854d0e6
Stein Arne Storslett <sastorsl@users.noreply.github.com>
2023-11-24 13:43:21 +0100

    Ensure HOME bash_completions are sourced.

---
+++ bin/bash-completion-d.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Ensure that a $HOME/.bash_completion and $HOME/.bash_completion.d/ exists and that
+# $HOME/.bash_completion.d/*.bash is sourced in $HOME/.bash_completion
+#
+# Download and run with
+# curl -sS https://raw.githubusercontent.com/sastorsl/scripts/main/bin/bash-completion-d.bash | bash
+#
+
+if [ -z "${BASH_VERSINFO}" ]
+then
+    echo "This script is intended to be run with bash. Aborting..."
+    exit 1
+fi
+
+if [ ! -f ${HOME:?}/.bash_completion ]
+then
+    touch ${HOME:?}/.bash_completion
+fi
+
+if [ ! -d ${HOME:?}/.bash_completion.d ]
+then
+    mkdir ${HOME:?}/.bash_completion.d
+fi
+
+if ! grep -q bash_completion.d ${HOME:?}/.bash_completion
+then
+    echo 'NG=$(shopt -p nullglob) ; shopt -s nullglob ; for FILE in $HOME/.bash_completion.d/*.bash; do   source $FILE; done ; ${NG}' >> ${HOME:?}/.bash_completion
+fi
